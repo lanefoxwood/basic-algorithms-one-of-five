5b69f8e167e174ef32457a9898082473eaff3501
lanefoxwood <lanefoxwood@gmail.com>
2022-09-09 16:45:48 +0200

    adding comments

--- CtoF.js
+++ CtoF.js
@@ -10,3 +10,19 @@ The formula to convert from Celsius to Fahrenheit is the temperature in Celsius 
 
 You are given a variable celsius representing a temperature in Celsius. Use the variable fahrenheit already defined and assign it the Fahrenheit temperature equivalent to the given Celsius temperature. Use the formula mentioned above to help convert the Celsius temperature to Fahrenheit.
 */
+
+// adding comments to this code which outline my problem solving logic
+
+
+// this function should convert Celsius to fahrenheit and (visa versa as a bonus)
+
+
+//TODO: find the math formula for converting a celsius number to a fahrenheit number and put it here:
+
+//TODO: define this function, which accepts a number, temp, temperature in celsius:
+function CtoF(temp){
+  //TODO: insde this function I need to put the math formula
+  // I need to define a variable, fahrenheit, which is the end result of the math formula
+  // I need to have this function return a value, fahrenheit, and display it
+  // then my temp conversion of a number in C to a number in F will be done!
+}
